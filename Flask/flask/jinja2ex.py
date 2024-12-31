aadd82e09c01809fcc3f85221e6215eb865f56ab
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-31 20:03:15 +0600

    Dynamic URL

--- Flask/flask/jinja2ex.py
+++ Flask/flask/jinja2ex.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def result(score):
         res = "Passed"
     else:
         res = "Failed"
-    return render_template("result.html", results=res)
+    return render_template("finalresult.html", results=res)
 
 
 @app.route("/resultfor/<int:score>")
@@ -22,6 +22,20 @@ def resultfor(score):
         res = "Failed"
     exp = {"score": score, "result": res}
     return render_template("result.html", results=exp)
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    total_score = 0
+    if request.method == "POST":
+        science = float(request.form["science"])
+        maths = float(request.form["maths"])
+        c = float(request.form["c"])
+        data_science = float(request.form["datascience"])
+        total_score = (science + maths + c + data_science) / 4
+    else:
+        return render_template("submit.html")
+    return redirect(url_for("resultfor", score=total_score))
 
 
 if __name__ == "__main__":
