5ce3ae09e49f0776fe1a0324686884571bc2e365
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-31 12:21:51 +0600

    Simple jinja2 with for loops

--- Flask/flask/jinja2ex.py
+++ Flask/flask/jinja2ex.py
@@ -13,5 +13,16 @@ def result(score):
     return render_template("result.html", results=res)
 
 
+@app.route("/resultfor/<int:score>")
+def resultfor(score):
+    res = ""
+    if score > 50:
+        res = "Passed"
+    else:
+        res = "Failed"
+    exp = {"score": score, "result": res}
+    return render_template("result.html", results=exp)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
